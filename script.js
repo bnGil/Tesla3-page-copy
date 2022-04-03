aca97fc91e8d190d9cb968b0b0b4ff58941cb59c
bnGil <gilbn1994@gmail.com>
2022-04-03 09:24:30 +0300

    change html file name to index.html

--- script.js
+++
@@ -1,9 +0,0 @@
-const navbar = document.getElementById("main-navbar");
-
-window.onscroll = function () {
-  if (window.pageYOffset > 100) {
-    navbar.classList.remove("top");
-  } else {
-    navbar.classList.add("top");
-  }
-};
